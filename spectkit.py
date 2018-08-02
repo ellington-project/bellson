2b887a6f26103e9c9ab096e84149d1b1bd187f70
Adam Harries <adam.harries@codeplay.com>
2018-08-02 16:53:10 +0100

    remove old functions, and include first minute to get more data

--- spectkit.py
+++ spectkit.py
@@ -14,44 +14,7 @@ import os
 import math
 from pathlib import Path
 
-# Extract audio data from a sample
-#     audiodata = numpy float array
-#     samplerate = int
-#     start = seconds
-#     length = seconds
-def extract_audio(audiodata, samplerate, start=60, length=60):
-    if start == None or length == None: 
-        return audiodata
-    # get the length of the audio data.
-    duration = librosa.core.get_duration(audiodata)
-    # Check to see that start + length < duration 
-    if start + length > duration: 
-        raise ValueError('Requested time longer than audio duration', str(start), str(length), str(duration)) 
-    # Calculate where the start would be, based on the sample rate
-    startframe = start * samplerate
-    # Calculate where the end would be, based on the sample rate
-    endframe = (start + length) * samplerate
-    # Return a slice of the numpy array
-    return audiodata[startframe:endframe]
-    
-
-# Save a segment of audio data to a spectrogram file
-def save_spectrogram(audio_dat, filename):
-    import matplotlib.pyplot as plt
-    print("Saving spectrogram to " + filename)
-    fig = plt.figure(figsize=(12, 12))
-    S = librosa.stft(audio_dat)
-    M = librosa.core.magphase(S)[0]
-    D = librosa.amplitude_to_db(M, ref=np.max)
-    ax = plt.subplot(111)
-    ax.set_frame_on(False)
-    plt.axis('off')
-    ax.axes.get_xaxis().set_visible(False)
-    ax.axes.get_yaxis().set_visible(False)
-    librosa.display.specshow(D, y_axis='linear')
-    fig.savefig(filename, dpi=100, bbox_inches='tight', pad_inches=0.0)
-    plt.close(fig)
-
+#                       (String  , numpy arr, int       , int  , int   )
 def spectrogram_interval(filename, audiodata, samplerate, start, length): 
     # get the length of the audio data.
     duration = librosa.core.get_duration(audiodata)
@@ -134,7 +97,7 @@ class Track:
         return "T["+str(self.bpm) + "," + str(self.filename) + "," + str(self.trackname) + "]"
 
     def interval_spect_path(self, start, end): 
-        return self.spect_path + self.digest + "-"+ str(start) + "-" + str(end) + ".png"
+        return self.spect_path + self.digest + "-"+ str(start) + "-" + str(end) + ".jpg"
 
     # Write spectrograms of this file, of 1 minute length, starting from 1 minute into the track, 
     # at an interval of 1 second
@@ -147,7 +110,7 @@ class Track:
         if duration < 60: 
             print("File to short to extract 1 minute of audio")
             return 
-        start = int(math.floor(min(60, duration-60)))
+        start = int(math.floor(min(0, duration-60)))
         stop = int(math.floor(duration))-60
         # Check for tracks < 1m
         # Iterate over 1s intervals 
