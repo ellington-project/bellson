03e28f0e7ab172f3c4f7d8e21b5761d1afa1889c
Adam Harries <harries.adam@gmail.com>
2020-05-12 20:04:04 +0100

    Remove old code in preparation for 2.0 code

--- bellson/audio/__init__.py
+++
@@ -1,3 +0,0 @@
-from .configuration import Configuration
-from .track import Track, TrackLoadException, CacheLevel
-from .trackiterator import TrackIterator, RangeError
--- bellson/util/__init__.py
+++
@@ -1,2 +0,0 @@
-from .ellib import EllingtonLibrary, LibEntry
-from .path import Path
